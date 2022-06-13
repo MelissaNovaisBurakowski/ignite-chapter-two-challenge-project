dbd961c2f00d5d40b8a806c2f8654df8e24d9d2f
Mel Novais Burakowski <mf.novais.98@gmail.com>
2022-06-13 19:57:08 -0300

    feat: use cases implemented

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -3,11 +3,17 @@ import { Request, Response } from "express";
 import { ListAllUsersUseCase } from "./ListAllUsersUseCase";
 
 class ListAllUsersController {
-  constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
+	constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
-  handle(request: Request, response: Response): Response {
-    // Complete aqui
-  }
+	handle(request: Request, response: Response): Response {
+		try {
+			const { user_id }: any = request.headers;
+			const allUsers = this.listAllUsersUseCase.execute({ user_id });
+			if (allUsers) return response.status(200).json(allUsers);
+		} catch (error) {
+			return response.status(400).json({ error: error.message });
+		}
+	}
 }
 
 export { ListAllUsersController };
